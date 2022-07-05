d448fc5b5a57059f03356c6afe3ea2dc849f314f
sanju <sanjusanju20546@gmail.com>
2022-07-05 16:46:15 +0000

    first changes from aws appserviceprovider conflict solved

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+
         // URL::forceScheme('https');
+=======
+	    Schema::defaultStringLength(191);
+	    
+>>>>>>> adb6cd4 (first changes from aws)
     }
 
 }
