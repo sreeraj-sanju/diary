f2347fe8ff17655ae3a9809ace1d680d9e0bb33d
sanju <sanjusanju20546@gmail.com>
2022-08-26 22:29:03 +0530

    conflicts solved

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\Checkrole::class,
-        'user' => \App\Http\Middleware\Checkuser::class,
+        'user' => \App\Http\Middleware\Checkuser::class
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -23,18 +23,20 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    // Route::get('/dashboard', function () {
-    //     return view('profile.show');
-    // })->name('dashboard');
-    //Route::view('dashboard', 'profile.show')->name('profile');
-});
 
 Route::middleware(['role:admin'])->group(function(){
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
     Route::view('boostrap-modal','super_admin/layout');
 
-    Route::view('budget','livewire.budget.expense_layout')->name('budget');
-    Route::view('stocks','livewire.stocks.stocks_layout')->name('stocks');
-    Route::view('components', 'livewire.electronics.components_layout')->name('component');
-    Route::view('bills', 'livewire.electronics.bills_layout')->name('bills');
+        Route::view('budget','livewire.budget.expense_layout')->name('budget');
+        Route::view('stocks','livewire.stocks.stocks_layout')->name('stocks');
+        Route::view('components', 'livewire.electronics.components_layout')->name('component');
+        Route::view('bills', 'livewire.electronics.bills_layout')->name('bills');
+    });
+    
+    Route::group(['middleware' => 'role:user'], function(){
+        Route::get('/profile', function () {
+                return view('profile.show');
+            })->name('profile');
+    });
 });
