c99bc5f555f283f4ad5a8fde63850053cb98f846
athulsree <athulsaas@gmail.com>
2022-08-26 15:02:05 +0530

    bash file modified

--- public/laravel-main.sh
+++ public/laravel-main.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-cyan = `tput setaf6`
-green = `tput setaf2`
-yellow = `tput setaf3`
-reset = `tput sgr0`
+cyan= `tput setaf 6`
+green= `tput setaf 2`
+yellow= `tput setaf 3`
+reset= `tput setaf 5`
 
 cd ~/Desktop/sreeraj/diary/
-echo '${green}Entered in to the laravel project folder${reset}'
+ echo "${green}Entered in to the laravel project folder${reset}" 
+
 git checkout sreeraj_nic
 git status
-echo "${cyan}Enter the number of files want to be add ${green}// if 0 add all files${reset}"
+echo "${cyan}Enter the number of files want to be add${yellow} // if 0 add all files${reset}"
 read count
 if [ $count -eq 0 ]
 then
 	git add .
-	echo "${green}add all the files${reset}"
+	echo "${green}added all the files${reset}"
 else
 	while [ $count -gt 0 ];
 	do
-    		echo "${cyan}Add the file ${reset}"
+    		echo "${cyan}Add the file${reset}"
 		read file
 		git add $file
 		((count--))
@@ -27,18 +28,18 @@ fi
 git status
 echo "${cyan}Enter the commit message${reset}"
 read msg
- git commit -m $msg
+ git commit -m "${msg##*  }"
 echo "${green}Committed successfully and start to push...${reset}"
 
 git push https://sreeraj-sanju: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@github.com/sreeraj-sanju/diary.git sreeraj_nic
 
 echo "${green}pushed successfully${reset}"
-echo "${yellow}branch checked to main and start to push${reset}"
+echo "${green}branch checked to main and start to push${reset}"
 git stash
 git checkout main
 git pull origin main
 git pull origin sreeraj_nic
 git push https://sreeraj-sanju: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@github.com/sreeraj-sanju/diary.git main
-echo "${green}pushed to main branch and checked into sreeraj_nic${reset}"
+echo "${green}Pushed to main branch and checked into sreeraj_nic${reset}"
 git checkout sreeraj_nic
 git stash pop
