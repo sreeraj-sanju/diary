b9e6bae26223e9ced6d5c1dfbf0eaa5435ff61b7
sreeraj <sreerajs728@gmail.com>
2022-09-14 18:39:38 +0530

    second db added and configured

---
+++ database/migrations/2022_09_14_124921_create_blog_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('bloggerName', 100)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('password', 20)->nullable();
+            $table->binary('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_users');
+    }
+}
