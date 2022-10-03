6a99d5c96a0b2083c5821d69610c573d4b09a40b
sreeraj <sreerajs728@gmail.com>
2022-10-03 07:38:26 +0530

    marakkar scene modified

--- resources/views/prathibha/prathibha.blade.php
+++ resources/views/prathibha/prathibha.blade.php
@@ -202,7 +202,7 @@
 				</div>
 				<div class="modal-body">
 					<video width="100%" height="100%" controls>
-						<source src="{{URL::asset("/assets/marakkar.mp4")}}" type="video/mp4">
+						<source src="{{URL::asset("/assets/marakar.mp4")}}" type="video/mp4">
 					</video>
 				</div>
 			</div>
