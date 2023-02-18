447527296a7159d766d2769806e5d6f0ad54fc29
sreeraj <sreerajs728@gmail.com>
2023-02-18 12:01:30 +0530

    user and admin role permission added

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PrathibhaController;
+use App\Http\Controllers\StockController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExpenseController;
-use App\Http\Controllers\{
-    DashboardController, PrathibhaController,StockController
-};
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,34 +14,39 @@ use App\Http\Controllers\{
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('super_admin.login');
-});
+})->name('lo');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified'
+    'verified',
 ])->group(function () {
     // Route::get('/dashboard', function () {
     //     return view('super_admin.dashboard');
     // })->name('dashboard');
-    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
-});
 
-Route::middleware(['admin:admin'])->group(function(){
-    // Route::get('/admin-dashboard', [DashboardController::class, 'dashboard'])->name('admin_dashboard');
-    Route::view('boostrap-modal','super_admin/layout');
+    Route::middleware(['auth', 'admin:admin'])->group(function () {
+        Route::get('/admin/dashboard', [DashboardController::class, 'dashboard'])->name('admin_dashboard');
+        Route::view('boostrap-modal', 'super_admin/layout');
 
-    Route::view('budget','livewire.budget.expense_layout')->name('budget');
-    Route::view('stocks','livewire.stocks.stocks_layout')->name('stocks');
-    Route::view('components', 'livewire.electronics.components_layout')->name('component');
-    Route::view('bills', 'livewire.electronics.bills_layout')->name('bills');
-    Route::view('actions', 'livewire.actions.items_layout')->name('actions');
-    Route::view('to_do_list', 'livewire.to_do_list.list')->name('to_do_list');
-    Route::get('/stock_analysis', [StockController::class, 'stock_analysis'])->name('stock_analysis');
+        Route::view('budget', 'livewire.budget.expense_layout')->name('budget');
+        Route::view('stocks', 'livewire.stocks.stocks_layout')->name('stocks');
+        Route::view('components', 'livewire.electronics.components_layout')->name('component');
+        Route::view('bills', 'livewire.electronics.bills_layout')->name('bills');
+        Route::view('actions', 'livewire.actions.items_layout')->name('actions');
+        Route::view('to_do_list', 'livewire.to_do_list.list')->name('to_do_list');
+        Route::get('/stock_analysis', [StockController::class, 'stock_analysis'])->name('stock_analysis');
+    });
+
+    Route::middleware(['auth', 'usercheck:user'])->group(function () {
+        Route::get('/dashboard', function () {
+            return view('profile.show');
+        })->name('dashboard');
+    });
 });
 
 // ROUTE SET FOR PRATHIBHA
